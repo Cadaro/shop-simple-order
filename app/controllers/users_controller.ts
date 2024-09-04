6cd44dda5c047cbe92876a56420334b168a4250b
Jakub Tyryllo <Cadaro@users.noreply.github.com>
2024-09-04 19:31:03 +0200

    Update users_controller.ts

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,22 +1,23 @@
-import User from '#models/user'
-import { createUserValidator } from '#validators/user'
-import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user';
+import { createUserValidator } from '#validators/user';
+import type { HttpContext } from '@adonisjs/core/http';
 
 export default class UsersController {
-  /**
-   * Create user
-   */
   async store({ request, response }: HttpContext) {
-    const validatedUserData = await request.validateUsing(createUserValidator)
-    const dbSave = await User.create(validatedUserData)
+    const validatedUserData = await request.validateUsing(createUserValidator);
+    const dbSave = await User.create(validatedUserData);
 
-    response.status(200).send({ userId: dbSave.id })
+    if (!dbSave) {
+      return response.internalServerError();
+    }
+
+    return response.noContent();
   }
 
   async index({ auth, response }: HttpContext) {
     if (!auth.isAuthenticated) {
-      return response.unauthorized()
+      return response.unauthorized();
     }
-    return response.status(200).send(auth.user)
+    return response.status(200).send(auth.user);
   }
 }
